b8cae0e91ba933896f76ebce607c05ee0293d10c
otmanoulcaid <ooulcaid.1337@gmail.com>
2025-06-28 18:46:44 +0000

    google oauth initial logic

--- auth/app/src/controllers/OAuthController.js
+++ auth/app/src/controllers/OAuthController.js
@@ -5,13 +5,59 @@ export class OAuthController
         this.authService = authService;
     }
 
-    googleOauth (request, reply)
-    { 
-    
+    async googleOauthasync(request, reply)
+    {    
+        const queryParams = {
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            redirect_uri: 'http://localhost:3000/api/oauth/google/callback',
+            response_type: 'code',
+            scope: 'openid email profile',
+        };
+
+        const url = `https://accounts.google.com/o/oauth2/v2/auth?${new URLSearchParams(queryParams).toString()}`;
+        reply.redirect(url);
     }
-    
-    googleCallbackOauth (request, reply)
+
+    async googleCallbackOauth(request, reply)
     {
-        
+        if (!request.query.code)
+            return reply.code(400).send('Missing "code" from Google');
+        try {
+            const tokenRes = await fetch('https://oauth2.googleapis.com/token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new URLSearchParams({
+                    code: request.query.code,
+                    client_id: process.env.GOOGLE_CLIENT_ID,
+                    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+                    redirect_uri: 'http://localhost:3000/api/oauth/google/callback',
+                    grant_type: 'authorization_code'
+                }).toString()
+            });
+
+            const tokenData = await tokenRes.json();
+
+            if (!tokenData.access_token)
+                throw new Error('No access token received');
+
+            const userRes = await fetch('https://www.googleapis.com/oauth2/v3/userinfo', {
+                headers: {
+                    Authorization: `Bearer ${tokenData.access_token}`
+                }
+            });
+            const user = await userRes.json();
+            const formatedUser = {
+                username: user.name,
+                email: user.email,
+                pass: '',
+                avatar: user.picture
+            };
+            await request.fastify.userService.addUser(formatedUser)
+        } catch (err) {
+            console.error('OAuth callback error:', err);
+            reply.code(500).send('Error during OAuth process');
+        }
     }
 }
