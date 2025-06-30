fc016d0bb7ecf48afd343850afafee434d686862
otmanoulcaid <ooulcaid.1337@gmail.com>
2025-06-30 15:31:25 +0100

    update : add tournament history service

--- user/app/src/repositories/TournamentRepository.js
+++ user/app/src/repositories/TournamentRepository.js
@@ -9,6 +9,17 @@ class TournamentRepository
     {
         await this.db.insert('Tournament', data);
     }
+
+    async getAllTournaments()
+    {
+        const query = `
+            select * from tournament t
+            join player p
+            on p.tournament_id = t.id
+        `;
+
+        return await this.db.all(query);
+    }
 }
 
 export { TournamentRepository }
